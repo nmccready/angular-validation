eb9391b7c813e097de9313951e465b59326f2e43
nmccready <nemtcan@gmail.com>
2017-01-20 22:47:49 -0500

    revert karma

--- config/karma.conf.angular.js
+++ config/karma.conf.angular.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
 
     // enable / disable watching file and executing tests whenever any file changes
     // CLI --auto-watch --no-auto-watch
-    autoWatch: true,
+    autoWatch: false,
 
     // Start these browsers, currently available:
     // - Chrome
@@ -52,7 +52,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     // CLI --browsers Chrome,Firefox,Safari
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     // CLI --capture-timeout 5000
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
     // Auto run tests on start (when browsers are captured) and exit
     // CLI --single-run --no-single-run
-    singleRun: false,
+    singleRun: true,
 
     // report which specs are slower than 500ms
     // CLI --report-slower-than 500
